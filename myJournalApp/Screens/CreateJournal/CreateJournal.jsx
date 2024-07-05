1393b3a7e22360218c13a986fb3a6d84658cd7cc
Mohammed Abdiladif <mihabulle45a@gmail.com>
2024-07-05 20:53:42 +0300

    0x0A

--- myJournalApp/Screens/CreateJournal/CreateJournal.jsx
+++ myJournalApp/Screens/CreateJournal/CreateJournal.jsx
@@ -1,8 +1,15 @@
 import {View} from 'react-native'
 export default function Createjournal (){
+    /*
+    1 able to select the date from calender
+    2. add title
+    3.add the Content
+    4. add the category
+    5. the date and time will automaticaly be  added in the database 
+    */
     return(
         <View>
-            
+            <Text>This is where one will create the Journals</Text>
         </View>
     )
 }
--- myJournalDEPR/config/serverUrl.jsx
+++
@@ -1,2 +0,0 @@
-const baseUrl = "http://192.168.100.59:8080"
-module.exports = baseUrl
